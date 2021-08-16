c7e95fa07d40cd3adec10a8538db65a607ac4a56
KristianaBlake <kristiana.blake@gmail.com>
2021-08-16 13:15:04 -0500

    database can populate with 50 random camp titles

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@
 // this file is going to connect to mongoose and use the Campground model file
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 // this is logic here saying use our local development database OR if this is in production 
@@ -23,6 +24,10 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+// grabbing a random element out of an array 
+// we pass in an array and get back a random element from that array
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 // we are going to starty by deleting everything in the database 
 const seedDB = async () => {
     await Campground.deleteMany({});
@@ -31,7 +36,8 @@ const seedDB = async () => {
         // here we are going to pick a random number and use that number to pick a city
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
     }
