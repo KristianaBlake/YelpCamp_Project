9f1ccef26d849518bb76bc8b79efcb89f4048589
KristianaBlake <kristiana.blake@gmail.com>
2021-08-17 14:05:20 -0500

    created edit route and edit page

--- app.js
+++ app.js
@@ -38,19 +38,27 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+// create page 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
-
-app.get('/campgrounds/:id', async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground })
-});
 
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+})
+
+// show page 
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground })
+});
+
+// edit page 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground })
 })
 
 app.listen(3000, () => {
