45f6e94b6afdc4cec07919c563a540998850b751
KristianaBlake <kristiana.blake@gmail.com>
2021-08-18 15:15:54 -0500

    added images and descriptions to show page

--- seeds/index.js
+++ seeds/index.js
@@ -35,9 +35,13 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         // here we are going to pick a random number and use that number to pick a city
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, non quidem exercitationem temporibus natus ipsam? Aperiam illum quas ex, dolorem cum quaerat, error aliquid suscipit assumenda necessitatibus exercitationem, veritatis debitis?',
+            price
         })
         await camp.save();
     }
