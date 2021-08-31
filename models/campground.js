cf937d096afdb63e0e3c87afab30e1c2c21a8b44
KristianaBlake <kristiana.blake@gmail.com>
2021-08-31 14:56:01 -0500

    defined Review model and added Review Form to campgrounds show page

--- models/campground.js
+++ models/campground.js
@@ -7,7 +7,14 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number, 
     description: String, 
-    location: String
+    location: String,
+    reviews: [
+        {
+            // this is an Object Id from the Review Model
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
