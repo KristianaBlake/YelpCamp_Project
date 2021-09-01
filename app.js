a42daa2595a757c832fe9a1b77ca75d59c35e4cd
KristianaBlake <kristiana.blake@gmail.com>
2021-08-31 21:41:37 -0500

    added delete route to delete reviews and corresponding campground id

--- app.js
+++ app.js
@@ -100,6 +100,16 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    // using mongo operator called "pull" to grab that *one* campground object id 
+    // from an array of object ids 
+    const { id, reviewId } = req.params;
+    // the $pull mongo operator will pull out any matching reviewIds from the array of reviews ids 
+    await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
 // show page 
 app.get('/campgrounds/:id', catchAsyncError(async(req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
