6218543fde9b2c0444957212377e3e6a677a33a9
KristianaBlake <kristiana.blake@gmail.com>
2021-09-24 14:41:18 -0500

    finished setting up attributes for cookies like expiration date, etc.

--- app.js
+++ app.js
@@ -47,6 +47,11 @@ const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false, 
     saveUninitialized: true,
+    cookie: {
+        httpOnly: true, 
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }
 app.use(session(sessionConfig))
 
