49a4a34ef0bdeec6275ac46827dd13fd8edf0a6b
KristianaBlake <kristiana.blake@gmail.com>
2021-10-27 16:37:48 -0500

    ALL users logic successfully transfered to users controller

--- routes/users.js
+++ routes/users.js
@@ -4,49 +4,22 @@ const passport = require('passport');
 const User = require('../models/user');
 const { reviewSchema } = require('../schemas');
 const catchAsyncError = require('../utils/catchAsync');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsyncError(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if(err) return next(err);
-            req.flash('success','Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
+router.post('/register', catchAsyncError(users.register));
 
 // serves login form
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.get('/login', users.renderLogin);
 
 // the actual logging in and checking to make sure credentials are valid
 
 // the "local" in possport authenticate, means the local browser
 // faiureFlash: gonna flash a message for us automatically
 // failureRedirect: if things go wrong, redirect to the login page again
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), users.login);
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-
-})
+router.get('/logout', users.logout);
 
 module.exports = router; 
