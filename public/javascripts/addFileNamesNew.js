583fd2028bb30bbfbd6f417a7c6bca2c6d4de9e6
KristianaBlake <kristiana.blake@gmail.com>
2021-11-02 15:40:52 -0500

    images can be seen before they are uploaded

--- public/javascripts/addFileNamesNew.js
+++ public/javascripts/addFileNamesNew.js
@@ -2,12 +2,11 @@
 // it will show the images, one by one, with the links of the
 // images uploaded 
 
-function previewMultiple(e) {
+function previewMultiple(event) {
     var images = document.getElementById("image");
     var number = images.files.length;
-    
-    for(i = 0;  i< number; i++) {
-        var urls = URL.createObjectURL(e.target.files[i]);
+    for (i = 0; i < number; i++) {
+        var urls = URL.createObjectURL(event.target.files[i]);
         document.getElementById("formFile").innerHTML += '<img src="' + urls + '">';
     }
 }
